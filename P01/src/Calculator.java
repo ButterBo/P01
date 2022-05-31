99765d16f656bfd0b688859da2758d655ac6bf20
21044912 <21044912@LAPTOP-22JOCTNR>
2022-05-31 12:26:09 +0800

    Testing I guess

---
+++ P01/src/Calculator.java
@@ -0,0 +1,14 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int add (int a, int b) {
+		 return a + b;
+		}
+
+	
+}
